1377ddc1c9a4c4b5f071a1a963cdaab70f23400e
Guddu Pandit <guddukrp769@gmail.com>
2025-02-21 23:15:54 +0530

    first commit

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LandingPage from './components/Landingpage.jsx'
+import Navigation from './Components/Navigation.jsx'
+
+
+function App() {
+return (
+  <div>
+    <Navigation/>
+    <LandingPage/>
+  </div>
+)
+}
+
+export default App
---
+++ Project-1/src/components/Landingpage.jsx
@@ -0,0 +1,23 @@
+const LandingPage = () => {
+  return (
+    <div className="main">
+      <div className="content">
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni
+          provident illum veritatis itaque voluptatem fugiat laborum. Odio
+          consequuntur fugiat quis, veritatis earum laudantium exercitationem
+          praesentium?
+        </p>
+        <div className="BtnMain">
+        <button>Book now</button>
+        <button className="btn">Category</button>
+        </div>
+      </div>
+      <div className="contentImage">
+        <img src="/header.png" alt="" />
+      </div>
+    </div>
+  );
+};
+export default LandingPage;
